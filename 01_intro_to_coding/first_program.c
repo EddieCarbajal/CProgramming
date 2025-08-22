2522e085cd711374fe15b7514734bfdd9a066338
Edgar Carbajal <ecarbajal3543@floridapoly.edu>
2025-08-22 14:36:11 +0000

    Created the first program

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,24 @@
+/*
+
+Multi-line comment.
+08/20/2025
+Edgar Carbajal
+The first program in C.
+
+*/
+
+// One line comment
+
+// Preporcessot directive
+// stdio.h - standars input output
+// .h - header file. A file with a description of something
+#include <stdio.h>  
+
+// Create the main function
+int main(void) {
+    printf("Hello\nWorld!\n"); // \n (new line)- escape sequence
+
+    printf("I\nam\nready\nto\ncode!\n");
+    
+    return 0;
+}
